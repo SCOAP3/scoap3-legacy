29f297516c13307f1ba24fabaf9f7ebddd49e2be
Samuele Kaplun <samuele.kaplun@cern.ch>
2014-02-05 11:37:54 +0100

    Nice SCOAP<sup>3</sub> title

--- webstyle_templates_scoap3.py
+++ webstyle_templates_scoap3.py
@@ -184,7 +184,7 @@ template function generated it.
  <tr>
   <td align="right" valign="top" colspan="12">
   <div class="headerboxbodylogo">
-   <a href="%(cssurl)s/?ln=%(ln)s">%(sitename)s</a>
+   <a href="%(cssurl)s/?ln=%(ln)s">SCOAP<sup>3</sup> Repository</a>
   </div>
   </td>
  </tr>
