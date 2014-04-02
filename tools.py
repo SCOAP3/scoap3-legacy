496bfa1b2dd7d33666805571b8a4f9f6aa12ce98
Wojciech Ziolek <wojciech.ziolek@cern.ch>
2014-04-02 12:22:51 +0200

    Adds new links to tools.py

--- tools.py
+++ tools.py
@@ -29,6 +29,8 @@ def index(req):
 
     req.write("<h2>National statistics</h2>")
     req.write("<a href='/nations.py'>Countries impact</a> - number of pulications per country<br />")
+    req.write("<a href='/nations.py/us_affiliations'>US affiliations</a> - all US affiliations<br />")
+    req.write("<a href='/nations.py/us_affiliations_csv'>Selected US aff count</a> - affiliation count for selected US universities<br />")
 
     req.write("<h2>Export to INSPIRE</h2>")
     req.write("<a href='/ffts_for_inspire.py'>Data export</a><br />")
