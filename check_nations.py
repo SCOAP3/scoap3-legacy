80a01f22e7a70f97a3764f1943b4fae4d0034772
Martin Vesper <martin.vesper@cern.ch>
2014-08-04 13:06:10 +0200

    Bibcheck: Adds check_nations plugin
    * adds flatten_list and has_field to utils.py
    
    * adds the bibcheck_task rule to rule.cfg and rule.cfg to the
      Makefile
    
    Signed-off-by: Martin Vesper <martin.vesper@cern.ch>

---
+++ check_nations.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+import collections
+from invenio.utils import flatten_list, has_field
+
+RECORD_FIELD = '700'
+RECORD_SUBFIELD = 'z'
+
+NATIONS_DEFAULT_MAP = {"Algeria": "Algeria",
+                       "Argentina": "Argentina",
+                       "Armenia": "Armenia",
+                       "Australia": "Australia",
+                       "Austria": "Austria",
+                       "Azerbaijan": "Azerbaijan",
+                       "Belarus": "Belarus",
+                       ##########BELGIUM########
+                       "Belgium": "Belgium",
+                       "Belgique": "Belgium",
+                       #######################
+                       "Bangladesh": "Bangladesh",
+                       "Brazil": "Brazil",
+                       "Bulgaria": "Bulgaria",
+                       "Canada": "Canada",
+                       "CERN": "CERN",
+                       "Chile": "Chile",
+                       ##########CHINA########
+                       "China (PRC)": "China",
+                       "PR China": "China",
+                       "China": "China",
+                       #######################
+                       "Colombia": "Colombia",
+                       "Costa Rica": "Costa Rica",
+                       "Cuba": "Cuba",
+                       "Croatia": "Croatia",
+                       "Cyprus": "Cyprus",
+                       "Czech Republic": "Czech Republic",
+                       "Denmark": "Denmark",
+                       "Egypt": "Egypt",
+                       "Estonia": "Estonia",
+                       "Finland": "Finland",
+                       "France": "France",
+                       "Georgia": "Georgia",
+                       ##########GERMANY########
+                       "Germany": "Germany",
+                       "Deutschland": "Germany",
+                       #######################
+                       "Greece": "Greece",
+                       ##########HONG KONG########
+                       "Hong Kong": "Hong Kong",
+                       "Hong-Kong": "Hong Kong",
+                       #######################
+                       "Hungary": "Hungary",
+                       "Iceland": "Iceland",
+                       "India": "India",
+                       "Indonesia": "Indonesia",
+                       "Iran": "Iran",
+                       "Ireland": "Ireland",
+                       "Israel": "Israel",
+                       ##########ITALY########
+                       "Italy": "Italy",
+                       "Italia": "Italy",
+                       #######################
+                       "Japan": "Japan",
+                       ##########SOUTH KOREA########
+                       "Korea": "South Korea",
+                       "Republic of Korea": "South Korea",
+                       "South Korea": "South Korea",
+                       #######################
+                       "Lebanon": "Lebanon",
+                       "Lithuania": "Lithuania",
+                       "México": "México",
+                       "Montenegro": "Montenegro",
+                       "Morocco": "Morocco",
+                       ##########NETHERLANDS########
+                       "Netherlands": "Netherlands",
+                       "The Netherlands": "Netherlands",
+                       #######################
+                       "New Zealand": "New Zealand",
+                       "Norway": "Norway",
+                       "Pakistan": "Pakistan",
+                       "Poland": "Poland",
+                       "Portugal": "Portugal",
+                       "Romania": "Romania",
+                       ##########RUSSIA########
+                       "Russia": "Russia",
+                       "Russian Federation": "Russia",
+                       #######################
+                       "Saudi Arabia": "Saudi Arabia",
+                       "Serbia": "Serbia",
+                       "Singapore": "Singapore",
+                       "Slovak Republic": "Slovakia",
+                       ##########SLOVAKIA########
+                       "Slovakia": "Slovakia",
+                       "Slovenia": "Slovenia",
+                       #######################
+                       "South Africa": "South Africa",
+                       "Spain": "Spain",
+                       "Sweden": "Sweden",
+                       "Switzerland": "Switzerland",
+                       "Taiwan": "Taiwan",
+                       "Thailand": "Thailand",
+                       "Tunisia": "Tunisia",
+                       "Turkey": "Turkey",
+                       "Ukraine": "Ukraine",
+                       ##########ENGLAND########
+                       "United Kingdom": "UK",
+                       "UK": "UK",
+                       #######################
+                       "England": "England",
+                       ##########USA########
+                       "United States of America": "USA",
+                       "United States": "USA",
+                       "USA": "USA",
+                       #######################
+                       "Uruguay": "Uruguay",
+                       "Uzbekistan": "Uzbekistan",
+                       "Venezuela": "Venezuela",
+                       ##########VIETNAM########
+                       "Vietnam": "Vietnam",
+                       "Viet Nam": "Vietnam",
+                       #######################
+                       #########other#########
+                       "Peru": "Peru",
+                       "Kuwait": "Kuwait",
+                       "Sri Lanka": "Sri Lanka",
+                       "Kazakhstan": "Kazakhstan",
+                       "Mongolia": "Mongolia",
+                       "United Arab Emirates": "United Arab Emirates",
+                       "Malaysia": "Malaysia",
+                       "Qatar": "Qatar",
+                       "Kyrgyz Republic": "Kyrgyz Republic",
+                       "Jordan": "Jordan"}
+
+
+def _find_field(rec):
+    def closure((field, subfield)):
+        try:
+            return [f for f in rec[field][0][0] if subfield in f]
+        except KeyError:
+            return None
+
+    return closure
+
+
+def find_nations(rec):
+
+    possible_fields = map(_find_field(rec), [('700', 'v'),
+                                             ('700', 'u'),
+                                             ('100', 'v'),
+                                             ('100', 'u')])
+
+    possible_fields = filter(lambda x: x is not None, possible_fields)
+
+    _all = []
+    for s in flatten_list(possible_fields):
+        _all.extend([x.replace('.', '') for x in s.split(', ')])
+
+    result = map(NATIONS_DEFAULT_MAP.get,
+                 filter(lambda x: x in _all, NATIONS_DEFAULT_MAP.keys()))
+
+    return result
+
+
+def check_records(records, empty=False):
+    for record in records:
+        if not has_field(record, RECORD_FIELD, RECORD_SUBFIELD):
+            nations = find_nations(record)
+            if len(nations) == 1:
+                val = nations[0]
+            else:
+                val = 'HUMAN CHECK'
+
+            field = '{0}__{1}'.format(RECORD_FIELD, RECORD_SUBFIELD)
+            if RECORD_FIELD in record:
+                record.add_subfield((field, 0, 0), RECORD_SUBFIELD, val)
+            else:
+                record.add_field(field, value='',
+                                 subfields=[(RECORD_SUBFIELD, val)])
---
+++ utils.py
@@ -0,0 +1,105 @@
+from os import listdir
+from os.path import isfile
+from collections import Iterable
+
+from invenio.search_engine import get_record
+from invenio.bibdocfile import BibRecDocs
+
+
+#SCOAP3 related
+def get_doi(recid):
+    return sorted(get_record(recid)['024'][0][0],
+                  key=lambda x: 1 if x[0] == 'a' else 2)[0][1]
+
+
+def get_doc_ids(recid):
+    return [bibdoc.get_id() for bibdoc in BibRecDocs(recid).list_bibdocs()]
+
+
+def get_arxiv(recid):
+    return sorted(get_record(recid)['037'][0][0],
+                  key=lambda x: 1 if x[0] == 'a' else 2)[0][1]
+
+
+def get_publisher(recid):
+    return sorted(get_record(recid)['773'][0][0],
+                  key=lambda x: 1 if x[0] == 'a' else 2)[0][1]
+
+
+def get_latest_pdf(bibrec_latest_files):
+    for file in bibrec_latest_files:
+        if file.format == '.pdf' or file.format == '.pdf;pdfa':
+            return file
+    return None
+
+
+def get_rawtext_from_record_id(record_id):
+    bibrec = BibRecDocs(record_id)
+    bibdoc = get_latest_pdf(bibrec.list_latest_files())
+    try:
+        rawtext = bibdoc.bibdoc.get_text()
+    except:
+        return ''
+
+    return rawtext
+
+
+def amend_fields(record, field, changes_dict, comment=''):
+    """record : AmendableRecord"""
+    for position, value in record.iterfield(field):
+        key = value[:value.find(':')]
+        if key in changes_dict:
+            record.amend_field(position,
+                               '%s:%d' % (key, changes_dict[key]),
+                               comment)
+
+
+def add_fields(record, field, subfield, value_dict):
+    """record : AmendableRecord"""
+    for key, value in value_dict.iteritems():
+        record.add_field(field,
+                         value="",
+                         subfields=[(subfield, '%s:%d' % (key, value))])
+
+
+def has_field(rec, field, subfield):
+
+    def _has_field(field, subfield):
+        if subfield == field:
+            return True
+
+        if isinstance(field, Iterable) \
+           and not isinstance(field, basestring):
+            return any([_has_field(x, subfield) for x in field])
+
+        return False
+
+    try:
+        return _has_field(rec[field], subfield)
+    except KeyError:
+        return False
+
+
+#GENERAL
+def get_filenames_from_directory(dirname, _filter=''):
+    return [join(dirname, f) for f in listdir(dirname)
+            if isfile(join(dirname, f)) and _filter in f]
+
+
+def flatten_list(l):
+    tmp = []
+    if isinstance(l, Iterable) and not isinstance(l, basestring):
+        for sl in l:
+            tmp.extend(flatten_list(sl))
+    else:
+        return [l]
+
+    return tmp
+
+
+def build_path(file_path):
+    pass
+
+
+def split_path(file_path):
+    pass
