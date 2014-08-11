f7734147659656b8d62791590a1830fa2a62980d
Martin Vesper <martin.vesper@cern.ch>
2014-08-11 14:09:50 +0200

    Nations: Reworks nations.py to use the field 700__w
    * Changes nations.py to use the field 700__w for the nation
      counting
    
    * Moves NATIONS_DEFAULT_MAP to utils.py
    
    Signed-off-by: Martin Vesper <martin.vesper@cern.ch>

--- utils.py
+++ utils.py
@@ -1,10 +1,138 @@
+# -*- coding: utf-8 -*-
 from os import listdir
 from os.path import (isfile, join)
 from collections import Iterable
 
-
 from invenio.search_engine import get_record
 from invenio.bibdocfile import BibRecDocs
+
+#VARIABLES
+NATIONS_DEFAULT_MAP = {"Algeria": "Algeria",
+                       "Argentina": "Argentina",
+                       "Armenia": "Armenia",
+                       "Australia": "Australia",
+                       "Austria": "Austria",
+                       "Azerbaijan": "Azerbaijan",
+                       "Belarus": "Belarus",
+                       ##########BELGIUM########
+                       "Belgium": "Belgium",
+                       "Belgique": "Belgium",
+                       #######################
+                       "Bangladesh": "Bangladesh",
+                       "Brazil": "Brazil",
+                       "Bulgaria": "Bulgaria",
+                       "Canada": "Canada",
+                       "Chile": "Chile",
+                       ##########CHINA########
+                       "China (PRC)": "China",
+                       "PR China": "China",
+                       "China": "China",
+                       #######################
+                       "Colombia": "Colombia",
+                       "Costa Rica": "Costa Rica",
+                       "Cuba": "Cuba",
+                       "Croatia": "Croatia",
+                       "Cyprus": "Cyprus",
+                       "Czech Republic": "Czech Republic",
+                       "Denmark": "Denmark",
+                       "Egypt": "Egypt",
+                       "Estonia": "Estonia",
+                       "Finland": "Finland",
+                       "France": "France",
+                       "Georgia": "Georgia",
+                       ##########GERMANY########
+                       "Germany": "Germany",
+                       "Deutschland": "Germany",
+                       #######################
+                       "Greece": "Greece",
+                       ##########HONG KONG########
+                       "Hong Kong": "Hong Kong",
+                       "Hong-Kong": "Hong Kong",
+                       #######################
+                       "Hungary": "Hungary",
+                       "Iceland": "Iceland",
+                       "India": "India",
+                       "Indonesia": "Indonesia",
+                       "Iran": "Iran",
+                       "Ireland": "Ireland",
+                       "Israel": "Israel",
+                       ##########ITALY########
+                       "Italy": "Italy",
+                       "Italia": "Italy",
+                       #######################
+                       "Japan": "Japan",
+                       ##########SOUTH KOREA########
+                       "Korea": "South Korea",
+                       "Republic of Korea": "South Korea",
+                       "South Korea": "South Korea",
+                       #######################
+                       "Lebanon": "Lebanon",
+                       "Lithuania": "Lithuania",
+                       ##########MEXICO########
+                       "Mexico": "Mexico",
+                       "México": "Mexico",
+                       #######################
+                       "Montenegro": "Montenegro",
+                       "Morocco": "Morocco",
+                       ##########NETHERLANDS########
+                       "Netherlands": "Netherlands",
+                       "The Netherlands": "Netherlands",
+                       #######################
+                       "New Zealand": "New Zealand",
+                       "Norway": "Norway",
+                       "Pakistan": "Pakistan",
+                       "Poland": "Poland",
+                       "Portugal": "Portugal",
+                       "Romania": "Romania",
+                       ##########RUSSIA########
+                       "Russia": "Russia",
+                       "Russian Federation": "Russia",
+                       #######################
+                       "Saudi Arabia": "Saudi Arabia",
+                       "Serbia": "Serbia",
+                       "Singapore": "Singapore",
+                       "Slovak Republic": "Slovakia",
+                       ##########SLOVAKIA########
+                       "Slovakia": "Slovakia",
+                       "Slovenia": "Slovenia",
+                       #######################
+                       "South Africa": "South Africa",
+                       "Spain": "Spain",
+                       "Sweden": "Sweden",
+                       "Switzerland": "Switzerland",
+                       "Taiwan": "Taiwan",
+                       "Thailand": "Thailand",
+                       "Tunisia": "Tunisia",
+                       "Turkey": "Turkey",
+                       "Ukraine": "Ukraine",
+                       ##########ENGLAND########
+                       "United Kingdom": "UK",
+                       "UK": "UK",
+                       #######################
+                       "England": "England",
+                       ##########USA########
+                       "United States of America": "USA",
+                       "United States": "USA",
+                       "USA": "USA",
+                       #######################
+                       "Uruguay": "Uruguay",
+                       "Uzbekistan": "Uzbekistan",
+                       "Venezuela": "Venezuela",
+                       ##########VIETNAM########
+                       "Vietnam": "Vietnam",
+                       "Viet Nam": "Vietnam",
+                       #######################
+                       #########other#########
+                       "Peru": "Peru",
+                       "Kuwait": "Kuwait",
+                       "Sri Lanka": "Sri Lanka",
+                       "Kazakhstan": "Kazakhstan",
+                       "Mongolia": "Mongolia",
+                       "United Arab Emirates": "United Arab Emirates",
+                       "Malaysia": "Malaysia",
+                       "Qatar": "Qatar",
+                       "Kyrgyz Republic": "Kyrgyz Republic",
+                       "Jordan": "Jordan"}
 
 
 #SCOAP3 related
@@ -60,6 +188,12 @@ def flatten_list(l):
         return [l]
 
     return tmp
+
+
+def multi_replace(s, replaces):
+        for old, new in replaces:
+            s = s.replace(old, new)
+        return s
 
 
 def build_path(file_path):
